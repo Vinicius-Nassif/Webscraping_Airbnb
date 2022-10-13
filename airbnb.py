80082477e51d114821fe24efc9736571f1ff7364
Vinicius-Nassif <vinicius.nassif@gmail.com>
2022-10-13 18:23:49 -0300

    Versão Web Scraping 2.0

--- airbnb.py
+++ airbnb.py
@@ -7,73 +7,153 @@ from time import sleep
 import pandas as pd
 import re
 
-# Operando o Selenium
-options = Options()
-options.add_argument('window-size=maximized')  # Especifica as dimensões da janela aberta
-options.add_experimental_option("detach", True) # Manter o Chrome aberto
-## options.add_argument('--headless')  # Realiza a rotina sem abrir o navegador
+class WebScraping():
 
-# Indicando a automação do Chrome 
-navegador = webdriver.Chrome(options=options)
-navegador.get('https://www.airbnb.com')
+	def __init__(self, url, cidade, arquivo):
+		# Alocando argumentos
+		self.url = url
+		self.cidade = cidade
+		self.nome_arquivo = arquivo
 
-# Indica quantos segundos aguarda para continuar executando o código
-sleep(10)
+		# Inicializando objetos
+		self.site = None
+		self.navegador = None
+		#self.hospedagens = None
+		self.dados_hospedagens = []
 
-# Indentificando o botão de busca
-button = navegador.find_element(By.TAG_NAME, "button")
-button.click()
+		# Expecifica configurações de inicialização do navegador -- Selenium
+		self.options = Options()
+		self.options.add_argument('window-size=maximized')  # Especifica as dimensões da janela aberta
+		self.options.add_experimental_option("detach", True) # Manter o Chrome aberto
+		## self.options.add_argument('--headless')  # Realiza a rotina sem abrir o navegador
 
-sleep(3)
+		print('Web Scraping inicializado!')
 
-# Identificando o input e enviando a busca desejada
-input_place = navegador.find_element(By.XPATH, "//*[@id='bigsearch-query-location-input']")
-input_place.send_keys('Rio de Janeiro')
-input_place.submit()
+	def run(self):
+		# Execução sequencial de todas as fases do Web Scraping
+		# Fases da execução:
+		# 	1. Pesquisar pelo URL do airbnb
+		self.get_url()
+		print('get_url executado com sucesso')
+		#	2. Buscar pelo botão de pesquisa
+		self.clica_busca()
+		print('clica_busca executado com sucesso')
+		#	3. Pesquisar pela cidade desejada
+		self.input_cidade()
+		print('input_cidade executado com sucesso')
+		#	4. Laço de paginação
+		botao_existe = True
+		while botao_existe is True:
 
-sleep(3)
+			#	5. Interpretar o HTML da página
+			self.integracao_bs()
+			print('integracao_bs executado com sucesso')
+			#	6. Listar anúncios da página atual
+			hospedagens = self.identifica_anuncio() 
+			print('identifica_anuncio executado com sucesso:')
+			print()
+			#	7. Extração de dados dos anúncios
+			self.raspagem_dados(hospedagens)
+			print('raspagem_dados executado com sucesso:')
+			#	8. Clica no botão se existir, se não quebra o laço
+			sleep(2)
+			botao_existe = self.prox_pag()
+			sleep(5)
 
-# Integração com o BeautifulSoup
-headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36'}
-page_content = navegador.page_source
-site = BeautifulSoup(page_content, 'html.parser')
+		#	9. Criar a tabela com todos anúncios
+		self.criar_tabela()
+		print('criar_tabela executado com sucesso:')
+		print('Web Scraping concluído!')
 
-# Criação de uma lista a ser preenchida futuramente com um dataframe
-dados_hospedagens = [] 
+	def get_url(self):
+		# Indicando a automação do Chrome 
+		self.navegador = webdriver.Chrome(options=self.options)
+		self.navegador.get(self.url)
 
-# Identificação do atributo raiz do anúncio
-hospedagens = site.findAll('div', attrs={'itemprop': 'itemListElement'})
+		sleep(5)
 
-# Automação da busca das hospedagens 
-for hospedagem in hospedagens:
-	hospedagem_descricao = hospedagem.find('span', class_='tjbvqj3 dir dir-ltr').get_text()
-	hospedagem_detalhes = hospedagem.find('span', class_='dir dir-ltr').get_text()
-	hospedagem_valor = hospedagem.find('div', class_='p11pu8yw dir dir-ltr').get_text()
-	hospedagem_url = hospedagem.find('meta', attrs={'itemprop': 'url'})
-	hospedagem_avaliacao = hospedagem.find('span', class_='ru0q88m dir dir-ltr').get_text()
+	def clica_busca(self):
+		# Indentificando o botão de busca
+		button = self.navegador.find_element(By.TAG_NAME, "button")
+		button.click()
 
-	print('Descrição:', hospedagem_descricao)
-	print('Detalhes:', hospedagem_detalhes)
+		sleep(3)
 
-	# Regex para formatar a apresentação do valor
-	valor_regex = re.search("R\$(\d{1,})", hospedagem_valor).group(1)
+	def input_cidade(self):
+		# Identificando o input e enviando a busca desejada
+		input_place = self.navegador.find_element(By.XPATH, "//*[@id='bigsearch-query-location-input']")
+		input_place.send_keys(self.cidade)
+		sleep(1)
+		input_place.submit()
+		sleep(3)
 
-	print('Valor:', valor_regex)
-	print('Avaliação:', hospedagem_avaliacao)
+	def integracao_bs(self):
+		# Início da integração do BeautifulSoup
+		headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36'}
+		self.site = BeautifulSoup(self.navegador.page_source, 'html.parser')
 
-	#Adicionar a informação dentro do atributo content
-	hospedagem_url = hospedagem_url['content'] 
-	print('URL:', hospedagem_url)
+	def identifica_anuncio(self):
+		# Identificação do atributo raiz do anúncio
+		return self.site.findAll('div', attrs={'itemprop': 'itemListElement'})
+
+	def raspagem_dados(self, hospedagens):     
+		# Automação da busca das hospedagens 
+		for hospedagem in hospedagens:
+		
+			hospedagem_descricao = hospedagem.find('div', class_='t1jojoys dir dir-ltr').get_text()
+			hospedagem_detalhes = hospedagem.find('span', class_='t6mzqp7 dir dir-ltr').get_text()
+			hospedagem_leitos = hospedagem.find('span', class_='dir dir-ltr').get_text()   
+			hospedagem_valor = hospedagem.find('span', class_='_tyxjp1').get_text()
+			hospedagem_url = hospedagem.find('meta', attrs={'itemprop': 'url'})
+			hospedagem_avaliacao = hospedagem.find('span', class_='r1dxllyb dir dir-ltr').get_text()
 	
-	# Utilizado para quebra de linhas
-	print() 
+			print('Descrição:', hospedagem_descricao)
+			print('Detalhes:', hospedagem_detalhes)
+			print('Leitos:', hospedagem_leitos)
+			print('Valor:', hospedagem_valor)
+			print('Avaliação:', hospedagem_avaliacao)
+			
+			#Adicionar a informação dentro do atributo content
+			hospedagem_url = hospedagem_url['content'] 
+			print('URL:', hospedagem_url)
+	
+			# Utilizado para quebra de linhas
+			print() 
 
+			# Criação do DataFrame
+			self.dados_hospedagens.append([hospedagem_descricao,
+							  				hospedagem_detalhes,
+							  				hospedagem_leitos,
+							 				hospedagem_valor, 
+							  				hospedagem_avaliacao, 
+							  				hospedagem_url])
+		
+            
+	def prox_pag(self):
+		# Encontrar e executar paginação do website
+		try:
+			next_button = self.navegador.find_element(By.CLASS_NAME, '_1bfat5l')
+			# Estabelecida variável que identifica o botão como desativado e dá fim a paginação
+			botao_vazio = next_button.get_property('disabled')
+			if botao_vazio is True:
+				print("Raspagem encerrada com sucesso!")                
+				return False
+			sleep(2)
+			next_button.click()
+			return True
+		except Exception as e:
+			print(e)
 
-	# Criação do DataFrame
-	dados_hospedagens.append([hospedagem_descricao, hospedagem_detalhes, valor_regex, hospedagem_avaliacao, hospedagem_url])
+	def criar_tabela(self): 
+		# Criação do .xlsx							  
+		dados = pd.DataFrame(self.dados_hospedagens, columns=['Descrição', 
+													'Detalhes',
+													'Leitos', 
+													'Valor', 
+													'Avaliação', 
+													'URL'])
 
-# Criação do .xlsx
-dados = pd.DataFrame(dados_hospedagens, columns=['Descrição', 'Detalhes', 'Valor', 'Avaliação', 'URL'])
-dados.to_excel('hospedagens.xlsx', index=False)
+		dados.to_excel(self.nome_arquivo, index=False)
 
-
+airbnb = WebScraping(url='http://www.airbnb.com.br', cidade='Rio de Janeiro', arquivo='dados_hospedagens.xlsx')
+airbnb.run()
